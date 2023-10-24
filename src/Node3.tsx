dd7be43bab195c05ea7e02b6393786a7d3deabe5
sajjadabd <mynameissajjad@gmail.com>
2023-10-24 10:20:48 +0330

    useRef to find and target canvas

--- src/Node3.tsx
+++ src/Node3.tsx
@@ -1,11 +1,13 @@
-import React, { useState, useEffect, useRef } from "react";
+import { useState, useEffect, useRef } from "react";
 import * as d3 from "d3";
 
 const random = d3.randomNormal(0, 1);
 
 const Nodes3 = () => {
+  const counter = useRef(null);
+
   const [dataset, setDataset] = useState(
-    Array.from({ length: 80 }, () => [random(), random()])
+    Array.from({ length: 20000 }, () => [random(), random()])
   );
 
   const ref = useRef<SVGSVGElement | null>(null);
@@ -18,20 +20,18 @@ const Nodes3 = () => {
     const height = window.innerHeight;
 
     svg.attr("width", width).attr("height", height);
-
     //svg.attr("viewBox", [0, 0, width, height]);
 
     svg
       .append("defs")
       .append("style")
-
       .text(`circle.highlighted { stroke: orangered; fill: orangered; }`);
 
     svg.selectAll("g").remove();
     const g = svg.append("g");
 
-    const x = d3.scaleLinear([0, 1], [0, 100]);
-    const y = d3.scaleLinear([0, 1], [0, 100]);
+    const x = d3.scaleLinear([0, 1], [0, 1000]);
+    const y = d3.scaleLinear([0, 1], [0, 1000]);
 
     const delaunay = d3.Delaunay.from(
       dataset,
@@ -46,13 +46,14 @@ const Nodes3 = () => {
       .attr("cx", (d) => x(d[0]))
       .attr("cy", (d) => y(d[1]))
       .attr("fill", "#bfc9ca")
-      .attr("r", 1);
+      .attr("r", 3);
 
     let transform = d3.zoomIdentity;
 
     const zoom = d3.zoom().on("zoom", zoomed);
 
     function zoomed(event: any) {
+      console.log(event.transform.k);
       g.attr("transform", (transform = event.transform));
       //points.attr("transform", event.transform);
     }
@@ -63,7 +64,7 @@ const Nodes3 = () => {
       .on("pointermove", (event) => {
         const p = transform.invert(d3.pointer(event));
         const i = delaunay.find(...p);
-        //console.log(i);
+        //console.log(dataset[i]);
         points.classed("highlighted", (_, j) => i === j);
         d3.select(points.nodes()[i]).raise();
       })
